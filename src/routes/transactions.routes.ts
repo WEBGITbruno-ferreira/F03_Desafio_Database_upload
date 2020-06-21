8aa7b9c0e2fba7894f76bd7ebffca3fecc2e85ad
BRUNO FERREIRA <brunoalmeidaferreira@hotmail.com>
2020-06-20 21:41:16 -0300

    Vai começar  a brincadeira de  importar  arquivos, até    aqui  6 testes  ok  !

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,22 +1,44 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepo = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepo.find();
+
+  const balance = await transactionsRepo.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  // começo recuperando  dados.
+
+  const { title, value, type, category } = request.body;
+  const createTransacion = new CreateTransactionService();
+
+  const transaction = await createTransacion.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deletetransactions = new DeleteTransactionService();
+
+  await deletetransactions.execute(id);
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
